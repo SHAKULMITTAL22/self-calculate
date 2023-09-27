02e0f7375c7b284cc82e6f77ab6984358d34d1e5
SHAKULMITTAL22 <80421896+SHAKULMITTAL22@users.noreply.github.com>
2023-09-27 12:37:08 +0530

    Test generated by RoostGPT for test roost-test using AI Type Vertex AI and AI Model codechat-bison

---
+++ main/Main_a2e85e6415_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/SHAKULMITTAL22/self-calculate/helper"
+	"testing"
+)
+
+func TestMain_a2e85e6415(t *testing.T) {
+	// Test case 1: Positive integers
+	a := helper.Dontknow(1, 2)
+	expected := 2
+	if a != expected {
+		t.Errorf("Dontknow(1, 2) = %d, expected %d", a, expected)
+	} else {
+		fmt.Println("Test case 1 passed")
+	}
+
+	// Test case 2: Negative integers
+	a = helper.Dontknow(-1, -2)
+	expected = 2
+	if a != expected {
+		t.Errorf("Dontknow(-1, -2) = %d, expected %d", a, expected)
+	} else {
+		fmt.Println("Test case 2 passed")
+	}
+
+	// Test case 3: Zero
+	a = helper.Dontknow(0, 2)
+	expected = 0
+	if a != expected {
+		t.Errorf("Dontknow(0, 2) = %d, expected %d", a, expected)
+	} else {
+		fmt.Println("Test case 3 passed")
+	}
+
+	// Test case 4: Large numbers
+	a = helper.Dontknow(1000000000, 2000000000)
+	expected = 2000000000000000000
+	if a != expected {
+		t.Errorf("Dontknow(1000000000, 2000000000) = %d, expected %d", a, expected)
+	} else {
+		fmt.Println("Test case 4 passed")
+	}
+
+	// Test case 5: Floating point numbers
+	a = helper.Dontknow(1.5, 2.5)
+	expected = 3
+	if a != expected {
+		t.Errorf("Dontknow(1.5, 2.5) = %d, expected %d", a, expected)
+	} else {
+		fmt.Println("Test case 5 passed")
+	}
+}
